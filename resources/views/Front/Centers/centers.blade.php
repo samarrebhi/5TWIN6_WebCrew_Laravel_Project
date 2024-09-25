b25d463131ab25af1ee131afb15dd9e21aaf9cdb
oumaima <oumaimazanteni15@gmail.com>
2024-09-26 00:32:27 +0100

    Centers page style

--- resources/views/Front/Centers/centers.blade.php
+++ resources/views/Front/Centers/centers.blade.php
@@ -1,22 +1,27 @@
+@extends('Front/layout')
+@section('content') 
 
-
-@if('success')
-    <div>{{session('success')}}</div>
-@endif
-<ul>
-
-@foreach($centers as $center)
-<table>
-    <td>
-{{$center->name}}
-</td>
-</table>
-<a href="{{route('center.show',$center->id)}}">Details</a>
-<a href="{{route('center.edit',$center->id)}}">Edit</a>
-<hr>
-<form method="post" action="{{route('center.destroy',$center->id)}}">
-    @csrf 
-    @method('DELETE')
-<button type="submit">Delete</button>
-</form>
-@endforeach
+<div class="container my-5" style="padding-top: 120px;"> 
+    <div class="row justify-content-center">
+        @foreach($centers as $center)
+            <div class="col-md-4 mb-4">
+                <div class="card border-success shadow-sm">
+                    <div class="card-body">
+                        <h5 class="card-title text-success">{{ $center->name }}</h5>
+                        <p class="card-text text-muted">
+                            Address: {{ $center->address }} <br>
+                        </p>
+                        <a href="{{ route('center.show', $center->id) }}" class="btn btn-outline-success btn-sm">Details</a>
+                        <a href="{{ route('center.edit', $center->id) }}" class="btn btn-outline-warning btn-sm">Edit</a>
+                        <form method="POST" action="{{ route('center.destroy', $center->id) }}" style="display: inline-block;">
+                            @csrf 
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-outline-danger btn-sm" onclick="return confirm('Are you sure you want to delete this center?')">Delete</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
+@endsection
